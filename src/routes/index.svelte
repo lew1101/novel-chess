b278cccdcdf3a4c23561ed5afb07decf8c9d5bb4
lew1101 <kennethlew001@gmail.com>
2022-01-04 13:52:41 -0500

    minorfix: move game into index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import ChessBoard from '@components/chess/ChessBoard.svelte';
+    import { STARTING_FEN } from '@lib/chess/chess';
+
+    let fen = STARTING_FEN;
+    // let fen = '8/3P4/8/8/8/8/4p3/8 w KQkq - 0 1';
+</script>
+
+<div class="container">
+    <ChessBoard mode="INTERACTIVE" {fen} showNotation={true} debug={true} --font-size="18px" />
+</div>
+
+<style>
+    .container {
+        margin: auto;
+    }
+</style>
