ae2bc6d4b217ad0457d9d21c69881fe813ac05bc
lew1101 <kennethlew001@gmail.com>
2022-01-04 21:31:24 -0500

    add link to repo in website

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,39 @@
 <slot />
-<footer class="footer">Made by Kenneth Lew</footer>
+<footer class="footer">
+    <span>Made by Kenneth Lew</span>
+    <span
+        >Github Repo: <a href="https://github.com/lew1101/novel-chess"
+            >https://github.com/lew1101/novel-chess</a
+        ></span
+    >
+</footer>
 
 <style>
     .footer {
         position: fixed;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        box-sizing: border-box;
+        width: 100%;
+        left: 0;
         bottom: 0;
+        margin: 0;
+        padding: 0 10px;
+
         font-family: 'Roboto', sans-serif;
+    }
+
+    .footer > span {
+        margin: 5px;
+    }
+    .footer > span > a {
+        color: #f79531;
+    }
+
+    @media only screen and (orientation: portrait) {
+        .footer {
+            display: none;
+        }
     }
 </style>
